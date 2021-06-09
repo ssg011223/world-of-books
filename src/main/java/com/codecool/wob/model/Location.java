cdf13673994d95fe01796bd7092efc210223bba5
Solya Gergo <solya.gergo.sandor@gmail.com>
2021-06-09 16:55:09 +0200

    add: Location necessary JsonProperty annotations

--- src/main/java/com/codecool/wob/model/Location.java
+++ src/main/java/com/codecool/wob/model/Location.java
@@ -1,5 +1,6 @@
 package com.codecool.wob.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,11 +12,15 @@ import java.util.UUID;
 @NoArgsConstructor
 public class Location {
     private UUID id;
+    @JsonProperty(value = "manager_name")
     private String managerName;
     private String phone;
+    @JsonProperty(value = "address_primary")
     private String primaryAddress;
+    @JsonProperty(value = "address_secondary")
     private String secondaryAddress;
     private String country;
     private String town;
+    @JsonProperty(value = "postal_code")
     private String postalCode;
 }
