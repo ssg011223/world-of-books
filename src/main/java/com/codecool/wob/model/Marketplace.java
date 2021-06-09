d7ad119c210a1a39618b783f7cfca5381a9e37b4
Solya Gergo <solya.gergo.sandor@gmail.com>
2021-06-09 15:51:07 +0200

    add: Marketplace model class

---
+++ src/main/java/com/codecool/wob/model/Marketplace.java
@@ -0,0 +1,13 @@
+package com.codecool.wob.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Marketplace {
+    private int id;
+    private String name;
+}
